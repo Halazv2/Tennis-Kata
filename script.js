6fbcc5c979289f1643d86d7a3046702c6a4595d6
Halazv2 <halaziani2003@gmail.com>
2024-02-29 15:21:13 +0100

    add colors to log output

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -11,8 +10,8 @@ readline.question("Enter player one name: ", (playerOne) => {
 
     console.log("Running tests...");
 
-    // setting PLAYER_ONE and PLAYER_TWO are being set temporarily for the npm run test command. 
-    exec(`PLAYER_ONE=${playerOne} PLAYER_TWO=${playerTwo} npm run test src/TennisGame.test.js`, (error, stdout, stderr) => {
+    // setting PLAYER_ONE and PLAYER_TWO are being set temporarily for the npm run test command.
+    exec(`PLAYER_ONE=${playerOne} PLAYER_TWO=${playerTwo} npm run test src/TennisGame.test.js -- --colors `, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         return;
