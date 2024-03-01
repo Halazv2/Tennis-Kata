fa9a232f3b6c1b72093111822907caf91ef6f46e
Halazv2 <halaziani2003@gmail.com>
2024-03-01 10:45:19 +0100

    Refactor test structure and enhance test execution script
    This commit includes several changes to improve the test structure and execution:
    
    	1. Separated each group of tests into different files for better organization and readability.
    	2. Created a setup module to avoid repeating game setup code in each test file.
    	3. Updated the test execution script to run multiple test files.
    	4. Added verbose logging to the test execution script for detailed test output.

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ const readline = require("readline").createInterface({
 });
 const { exec } = require("child_process");
 
+const tests = ["src/tests/initialScore.test.js", "src/tests/advantage.test.js", "src/tests/douce.test.js", "src/tests/win.test.js"];
+
 readline.question("Enter player one name: ", (playerOne) => {
   readline.question("Enter player two name: ", (playerTwo) => {
     readline.close();
 
     console.log("Running tests...");
 
-    // setting PLAYER_ONE and PLAYER_TWO are being set temporarily for the npm run test command.
-    exec(`PLAYER_ONE=${playerOne} PLAYER_TWO=${playerTwo} npm run test src/TennisGame.test.js -- --colors `, (error, stdout, stderr) => {
+    /** setting PLAYER_ONE and PLAYER_TWO are being set temporarily for the npm run test command.
+     * @verbose flag is used to display the full test name in the console.
+     * @colors flag is used to display the test results in color.
+     */
+
+    exec(`PLAYER_ONE=${playerOne} PLAYER_TWO=${playerTwo} npm run test ${tests.join(" ")} -- --colors --verbose`, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         return;
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
+      console.log(`${stdout}`);
+      console.error(`${stderr}`);
     });
   });
 });
